ce8919531e56fb55dc252b92e81380fb72378fe6
Eddy21st <93651858+Eddy21st@users.noreply.github.com>
2021-11-29 13:29:01 +0200

    JS-HW11 DOM EVENTS
    Форма логина.

--- script.js
+++ script.js
@@ -1,99 +1,68 @@
-function createArr() {
-  let inputNumbers = prompt("Enter any numbers(min 3)");
-  const arr = inputNumbers.split(" ").map(Number);
-  for (let i = 0; i < arr.length; i++) {
-    while (isNaN(arr[i]) || arr.length < 3) {
-      arr[i] = +prompt("Enter correct values!");
+(() => {
+    const formEl = document.getElementById("form");
+    const emailEl = document.getElementById("emailInp");
+    const passwordEl = document.getElementById("passwordInp");
+    const btnEl = document.getElementById("btn");
+    const wrap = document.getElementById("wrap");
+    const errorEl = document.getElementById("error");
+    const errorPassword = document.getElementById("error-password");
+    const h2 = document.getElementById("h2");
+  
+    function submit() {
+      formEl.style.display = "none";
+      const alert = document.createElement("div");
+      wrap.append(alert);
+      alert.className = "alert";
+      alert.innerHTML = "Form submitted successfully! Congratulations :)";
     }
-  }
-  return arr;
-}
-//--------------------view------------------------//
-function viewResult(arr) {
-  alert(`Result is ${arr}`);
-  console.log(arr);
-}
-function divider(arr) {
-  if (arr.length > 1) {
-    let firstRequest = confirm("Do you wanna to divide your result?");
-    if (firstRequest == 1) {
-      let secondRequest = prompt(" Type your divider: ");
-      arr = arr.join(secondRequest);
-      showInfo(arr);
-      return arr;
+    // --------------------------Проверка на пустую строку---------------------------
+    function disable() {
+      if (emailEl.value === "" || passwordEl.value === "") {
+        btnEl.disabled = true;
+      } else {
+        btnEl.disabled = false;
+      }
     }
-  }
-  return arr;
-}
 
-//-------------------process----------------------//
-function arrOperations(arr) {
-  let selectOperator = +prompt(
-    "Choose operation: 1 - sort a-b, 2 - sort b-a, 3 - even, 4 -  odd, 5 - sum, 6 - mid, 7 - max, 8 - min"
-  );
-  switch (selectOperator) {
-    case 1:
-      let sortA = arr.sort((a, b) => a - b);
-      viewResult(sortA);
-      arr = divider(arr);
-      break;
-    case 2:
-      let sortB = arr.sort((a, b) => b - a);
-      viewResult(sortB);
-      arr = divider(arr);
-      break;
-    case 3:
-      let even = arr.filter((i) => i % 2 == 0);
-      viewResult(even);
-      break;
-    case 4:
-      let odd = arr.filter((i) => i % 2 !== 0);
-      viewResult(odd);
-      break;
-    case 5:
-      let sumResult = arr.reduce((sum, current) => sum + current, 0);
-      arr = divider(arr);
-      viewResult(sumResult);
-      break;
-    case 6:
-      let midResult = arr.reduce((sum, current) => sum + current, 0);
-      midResult = midResult / arr.length;
-      arr = divider(arr);
-      viewResult(midResult);
-      break;
-    case 7:
-      let max = Math.max.apply(Math, arr);
-      arr = divider(arr);
-      viewResult(max);
-      break;
-    case 8:
-      let min = Math.min.apply(Math, arr);
-      arr = divider(arr);
-      viewResult(min);
-      break;
-  }
-  return;
-}
-function main() {
-  let newCircle = 0;
-  do {
-    let oldCircle = 0;
-    const mainArray = createArr();
-    do {
-      finalResult = arrOperations(mainArray);
-      let newOps = confirm("Do you want to make new operations?");
-      if (newOps == 1) {
-        let chooseCircle = +prompt(`1 - make operations with an old array.
-                2 - make operations with a new array.`);
+    emailEl.addEventListener("blur", () => {
+      disable();
+      if (!emailEl.value.includes("@")) {
+        errorEl.innerHTML = "Enter correct email, please";
+        errorEl.style.color = "red";
+        emailEl.style.borderColor = "red";
+      }
+    });
+    emailEl.addEventListener("input", () => {
+      disable();
+    });
+  
+    passwordEl.addEventListener("blur", () => {
+      disable();
+      if (passwordEl.value === "") {
+        errorPassword.innerHTML = "Enter correct password, please";
+        errorPassword.style.color = "red";
+        passwordEl.style.borderColor = "red";
+        passwordEl.value = "";
+      }
+    });
+    passwordEl.addEventListener("input", () => {
+      disable();
+    });
 
-        if (chooseCircle === 1) {
-          oldCircle = 0;
-        } else if (chooseCircle === 2) {
-          oldCircle = 1;
-        }
-      } else return;
-    } while (oldCircle === 0);
-  } while (newCircle === 0);
-}
-
-main();
+    function check() {
+      if (!emailEl.value != "admin@mail.com" && passwordEl.value != "12345") {
+        const massege = document.createElement("div");
+        massege.innerHTML = "Form is not correct";
+        massege.style.color = "red";
+        h2.after(massege);
+      } else {
+        submit();
+      }
+    }
+  
+    btnEl.addEventListener("click", (event) => {
+      event.preventDefault();
+      emailEl.value = "";
+      check();
+    });
+  })();
